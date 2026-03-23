0bd62572b74f95eda713aa0d73cf3220942efd98
Chhavi0926 <chhavichhipa074@gmail.com>
2026-03-23 10:57:54 +0530

    Add files via upload

---
+++ if_else condition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Check if the number is even or odd
+    if (number % 2 == 0) {
+        cout << "The number is even." << endl;
+    } else {
+        cout << "The number is odd." << endl;
+    }
+
+    return 0;
+}
